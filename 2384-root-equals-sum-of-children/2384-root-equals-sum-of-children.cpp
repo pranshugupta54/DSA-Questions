814346ec135cf58f79b0c904cbfe79e5780334b2
Pranshu Gupta <pranshgupta54@gmail.com>
2023-10-12 01:18:32 +0530

    Time: 7 ms (7.01%), Space: 13 MB (12.39%) - LeetHub

--- 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.cpp
@@ -12,9 +12,6 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        if(root -> left -> val + root -> right -> val == root-> val){
-            return true;
-        }
-        return false;
+        return root->left->val+root->right->val==root->val;
     }
 };
