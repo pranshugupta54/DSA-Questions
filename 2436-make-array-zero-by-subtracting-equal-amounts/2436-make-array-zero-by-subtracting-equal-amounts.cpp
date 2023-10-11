7d7569a4e20da1c267e54a3be8dc95c2090b81dd
Pranshu Gupta <pranshgupta54@gmail.com>
2023-10-12 00:18:22 +0530

    Time: 2 ms (53.01%), Space: 8.7 MB (15.09%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         unordered_set<int> myset;
-        bool zero = false;
         for(int i = 0; i < nums.size(); i++){
-            myset.insert(nums[i]);
             if(nums[i] == 0){
-                zero = true;
+                continue;
             }
-        }
-        if(zero){
-            return myset.size() - 1;
+            myset.insert(nums[i]);
         }
         return myset.size();
     }
